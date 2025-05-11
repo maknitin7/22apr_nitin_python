64c13bd2433041233baa65be14933163101cc9b2
maknitin7 <nitinmakadiya7424@gmail.com>
2025-05-11 16:04:47 +0530

    Add files via upload

---
+++ ASSIGNMENTS/Module 1/Part 4/ex_6.py
@@ -0,0 +1,11 @@
+num= int(input(" Enter a number:"))
+
+if num<=1:
+    print(" Number is not a Prime number.")
+
+elif (num/2)!=0:
+    print("number is a Prime number.")
+    
+else:
+    print("invalid Number!!")
+    
